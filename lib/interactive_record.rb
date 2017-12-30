44c9f281938812e3f97fac8dc56bca0bae5ae0a5
Sherry El Garhy <sherryptk@gmail.com>
2017-12-30 22:38:43 +0000

    automatically backed up by learn

--- lib/interactive_record.rb
+++ lib/interactive_record.rb
@@ -8,18 +8,23 @@ class InteractiveRecord
   end
 
   def self.column_names
-  DB[:conn].results_as_hash = true
+      DB[:conn].results_as_hash = true
 
-  sql = "PRAGMA table_info('#{table_name}')"
+      sql = "PRAGMA table_info('#{table_name}')"
 
-  table_info = DB[:conn].execute(sql)
-  column_names = []
+      table_info = DB[:conn].execute(sql)
+      column_names = []
 
-  table_info.each do |column|
-    column_names << column["name"]
+      table_info.each do |column|
+        column_names << column["name"]
+    end
+
+    column_names.compact
   end
 
-  column_names.compact
-end
+  def initialize(options={})
+    options.each do |property, value|
+    self.send("#{property}=", value)
+  end
 
 end
